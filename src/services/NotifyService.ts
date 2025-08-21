c40fdd9cabdb78281a67abeaa03ea23006ad6908
AbdullahAlFalah <salem908mk@gmail.com>
2025-08-21 23:25:32 +0300

    tweaks 2

--- src/services/NotifyService.ts
+++ src/services/NotifyService.ts
@@ -1,4 +1,4 @@
-import notifee, { AndroidColor, AndroidImportance, AndroidVisibility, RepeatFrequency, TimestampTrigger, TriggerType } from '@notifee/react-native';
+import notifee, { AndroidColor, AndroidImportance, AndroidVisibility, NotificationAndroid, RepeatFrequency, TimestampTrigger, TriggerType } from '@notifee/react-native';
 import { ensureNotificationPermission } from '../utils/notificationsPermission';
 import { Platform } from 'react-native';
 
@@ -59,7 +59,7 @@ export async function scheduleNotification() {
     ];
 
     // Dynamic approach
-    const androidNotification: any = {
+    const androidNotification: NotificationAndroid = {
       channelId,
       smallIcon: 'ic_notification',
       showTimestamp: true, // shows system clock
@@ -73,9 +73,7 @@ export async function scheduleNotification() {
       androidNotification.color = '#9c27b0';
     } else if (Platform.OS === 'android' && Platform.Version >= 31) {
       // On Android 12+, force color using colorized
-      androidNotification.colorized = true;
       androidNotification.color = AndroidColor.CYAN;
-      // androidNotification.circularLargeIcon = true;
     }
 
     // Schedule the notification
